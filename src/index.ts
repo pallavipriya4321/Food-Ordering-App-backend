4dde179554205545713b1955f96b7af2348fdb52
Pallavi Priya <pallavipriya4321@gmail.com>
2025-07-11 20:41:03 +0530

    fetching api data and sending it to frontend

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .use(cors())
+  .get("/", () => "Hello Elysia")
+  .get("/restaurants", async () => {
+    const response = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.569035484544777&lng=73.88432573069704&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const data = await response.json();
+    return data;
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
